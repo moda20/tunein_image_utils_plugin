ff7fa5129b8a5695f71c715de026ad3c76995fc5
Moda20 <kadhem03@gmail.com>
2022-12-29 23:55:48 +0100

    analytics update

--- lib/tunein_image_utils_plugin.dart
+++ lib/tunein_image_utils_plugin.dart
@@ -4,7 +4,6 @@
 // directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
 
 import 'dart:async';
-import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 
